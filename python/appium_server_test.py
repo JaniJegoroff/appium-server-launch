81819d4659f0dcd61cfb1b7a603d9f792f1fad42
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-08-06 20:00:02 +0100

    refactor(python): cleanup

--- python/appium_server_test.py
+++ python/appium_server_test.py
@@ -4,12 +4,10 @@ import requests
 
 def test_appium_server_running(appium_server):
     """test Appium server is running"""
-
     assert appium_server.is_running
 
 def test_appium_server_listening(appium_server):
     """test Appium server is listening"""
-
     assert appium_server.is_listening
 
 def test_appium_server_http_status_code(appium_server_status_url):
