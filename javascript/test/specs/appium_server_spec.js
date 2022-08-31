bf9cbb1f823a96678c0860295a833b6d9cea9ad1
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-08-31 23:18:25 +0100

    refactor(javascript): use short-hand function definitions

--- javascript/test/specs/appium_server_spec.js
+++ javascript/test/specs/appium_server_spec.js
@@ -1,20 +1,20 @@
-const appium = require('appium')
-const chai = require('chai')
+const appium = require("appium");
+const chai = require("chai");
 
-const assert = chai.assert
+const assert = chai.assert;
 
-describe('Appium server', function () {
-    before(async function () {
-        let args = {base_path: '/wd/hub', allow_cors: true}
-        this.server = await appium.main(args)
-    });
+describe("Appium server", () => {
+  before(async () => {
+    let args = { base_path: "/wd/hub", allow_cors: true };
+    this.server = await appium.main(args);
+  });
 
-    after(async function () {
-        await this.server.close()
-    });
+  after(async () => {
+    await this.server.close();
+  });
 
-    it('should be running', async () => {
-        const res = await fetch('http://localhost:4723/wd/hub/status')
-        assert(res.status == 200)
-    });
+  it("should be running", async () => {
+    const res = await fetch("http://localhost:4723/wd/hub/status");
+    assert(res.status == 200);
+  });
 });
